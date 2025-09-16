7c30f38057d1d77f024e18812b04ef39948e2292
Luis Cardoso <luiscardoso@MacBook-Pro-de-Luis.local>
2025-09-16 14:23:47 -0300

    ci(workflows): change versioning to independent

--- packages/ds/src/lib/ds.tsx
+++ packages/ds/src/lib/ds.tsx
@@ -1,6 +1,7 @@
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export function SiulCoreDs() {
   return (
@@ -10,6 +11,8 @@ export function SiulCoreDs() {
       <Typography variant="body1">
         This is a design system package using Material-UI components.
       </Typography>
+      <Divider />
+      <CircularProgress variant="determinate" value={50} />
       <Button variant="contained" color="secondary">
         Click Me
       </Button>
