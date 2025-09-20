11b6b6400183fde95e14efa5a16599ae16f1c742
Luis Cardoso <luiscardoso@MacBook-Pro-de-Luis.local>
2025-09-20 17:04:13 -0300

    feat(ds-mui): force more changes

--- packages/ds-mui/src/components/Title/Title.tsx
+++ packages/ds-mui/src/components/Title/Title.tsx
@@ -3,7 +3,6 @@ import { Divider, Typography } from '@mui/material';
 export function Title() {
   return (
     <div>
-      <Divider />
       <Typography variant="h1" color="info">
         Welcome to Title!
       </Typography>
